345baf17777646faec71491d26ade20f40090048
Stephen Farley <stephen.farley@renaissance.com>
2020-12-02 11:18:02 -0600

    Initial Commit

---
+++ Mission001/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace TrackingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _input = File.ReadAllText("Input.txt");
+
+            for(int i = 0; i < _input.Length - 16; i++)
+            {
+                if (uniqueCharacters(_input.Substring(i, 16)))
+                {
+                    var data = Convert.FromBase64String(_input.Substring(i, 16));
+                    var str = System.Text.Encoding.ASCII.GetString(data);
+                    Console.WriteLine(str);
+                    break;
+                }
+            }
+        }
+
+        static bool uniqueCharacters(String str)
+        {
+            char[] chArray = str.ToCharArray();
+
+            // Using sorting
+            Array.Sort(chArray);
+
+            for (int i = 0; i < chArray.Length - 1; i++)
+            {
+
+                // if the adjacent elements are not
+                // equal, move to next element
+                if (chArray[i] != chArray[i + 1])
+                    continue;
+
+                // if at any time, 2 adjacent elements
+                // become equal, return false
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Mission002/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Mission002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var  j = JArray.Parse(File.ReadAllText("Output.txt"));
+            List<Day> days = new List<Day>();
+            foreach(JObject o in j)
+            {
+                Day d = new Day { date = o.First.First.ToString() };
+                //Console.WriteLine(o.First.First.ToString());
+                days.Add(d);
+            }
+            
+            // Convert initial input from binary
+            //string[] data = _input.Split(" ");
+            //byte[] bData = data.Select(d => Convert.ToByte(d,2)).ToArray();
+            //string s = "";
+            //foreach (byte b in bData) {
+            //    var x = Convert.ToChar(b);
+            //    //Console.Write(x);
+            //    s += x;
+            //}
+            //File.WriteAllText("Output.txt", s);
+        }
+    }
+}
+
+class Day
+{
+    public string date;
+    List<Reading> readings;
+}
+
+class Reading
+{
+    string id;
+    int time;
+    List<Contaminant> contaminants;
+}
+
+class Contaminant
+{
+    string id;
+    int amount;
+}
